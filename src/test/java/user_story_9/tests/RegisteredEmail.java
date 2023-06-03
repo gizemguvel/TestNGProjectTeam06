104e874b0b9499cf3d1306560c567473b825884b
gizemguvel <gguvel@hotmail.com>
2023-06-03 13:05:03 +0300

    commiting test ng project test

--- src/test/java/user_story_9/tests/RegisteredEmail.java
+++ src/test/java/user_story_9/tests/RegisteredEmail.java
@@ -1,7 +1,6 @@
 package user_story_9.tests;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WindowType;
 import org.testng.annotations.Test;
@@ -9,20 +8,18 @@ import user_story_9.pages.PearlyMarketLoginRegisterPage;
 import utilities.*;
 
 import java.io.IOException;
-import java.io.ObjectInputFilter;
 
-public class VendorRegister extends ExtentReportsBase {
+public class RegisteredEmail extends ExtentReportsBase {
     ExcelUtils excelUtils;
-
-
     @Test
-    public void vendorRegisterTest() throws IOException {
+    public void registeredEmail() throws IOException {
+
         String path="src/test/java/resources/account.xlsx";
         String sheetName="Sheet 1";
         excelUtils=new ExcelUtils(path,sheetName);
         PearlyMarketLoginRegisterPage pearlyMarketLoginRegisterPage=new PearlyMarketLoginRegisterPage();
 
-        //extentTest.pass("Start testing");
+        //extentTest.pass("start testing");
 //        Go to website https://pearlymarket.com/
         Driver.getDriver().get(ConfigReader.getProperty("pearly_market_url"));
         MediaUtils.takeScreenshotOfTheEntirePageAsString();
@@ -30,38 +27,36 @@ public class VendorRegister extends ExtentReportsBase {
         pearlyMarketLoginRegisterPage.registerLink.click();
 //        Click on Become a Vendor
         pearlyMarketLoginRegisterPage.becomeAVendorLink.click();
-       // extentTest.pass("register as a vendor");
+        //extentTest.pass("register as a vendor");
         MediaUtils.takeScreenshotOfTheEntirePageAsString();
 //        Enter the email
-        pearlyMarketLoginRegisterPage.registerEmail.sendKeys("gizem_1206@hotmail.com");
+        pearlyMarketLoginRegisterPage.registerEmail.sendKeys("gguvel@hotmail.com");
         pearlyMarketLoginRegisterPage.verificationCode.click();
-       // extentTest.pass("Verification code sent to your email is visible");
+        //extentTest.pass("Verification code sent to your email is visible");
 //        Verify that “Verification code sent to your email: guvelg@gmail.com.” is visible
         WaitUtils.waitForVisibility(pearlyMarketLoginRegisterPage.verificationCodeMessage,3);
         ReusableMethods.verifyElementDisplayed(pearlyMarketLoginRegisterPage.verificationCodeMessage);
         MediaUtils.takeScreenshotOfTheEntirePageAsString();
-
 //        Check your e-mail
         Driver.getDriver().switchTo().newWindow(WindowType.TAB);
         Driver.getDriver().get(ConfigReader.getProperty("outlook"));
         Driver.getDriver().findElement(By.xpath("(//nav//ul//li)[2]//a")).click();
-        Driver.getDriver().findElement(By.xpath("//input[@type='email']")).sendKeys("gizem_1206@hotmail.com");
+        Driver.getDriver().findElement(By.xpath("//input[@type='email']")).sendKeys("gguvel@hotmail.com");
         Driver.getDriver().findElement(By.xpath("//input[@type='submit']")).click();
-        Driver.getDriver().findElement(By.id("i0118")).sendKeys(excelUtils.getCellData(2,1));
+        Driver.getDriver().findElement(By.xpath("//input[@type='password']")).sendKeys(excelUtils.getCellData(2,0));
         Driver.getDriver().findElement(By.id("idSIButton9")).click();
         Driver.getDriver().findElement(By.id("idBtn_Back")).click();
-
-
 //        Get the verification code from your e-mail
         WaitUtils.waitForVisibility(By.xpath("//div//span[@title='info@pearlymarket.com']"),5).click();
         String verificationCode=Driver.getDriver().findElement(By.xpath("//*[@id='x_body_content_inner']/p[2]/b")).getText();
+
         //extentTest.pass("typing verification code");
 //        Type verification code
         ReusableMethods.switchToWindow(0);
         pearlyMarketLoginRegisterPage.verificationCode.sendKeys(verificationCode);
         MediaUtils.takeScreenshotOfTheEntirePageAsString();
 
-       // extentTest.pass("entering password");
+        //extentTest.pass("entering password");
 //        Enter password and confirm password(password should contain uppercase, lowercase, digit and special character)
         pearlyMarketLoginRegisterPage.registerPassword.sendKeys(ConfigReader.getProperty("vendor_password"));
         pearlyMarketLoginRegisterPage.confirmPassword.sendKeys(ConfigReader.getProperty("vendor_password"));
@@ -76,11 +71,17 @@ public class VendorRegister extends ExtentReportsBase {
         }else {
             System.out.println("password does not contain uppercase, lowercase, digit and special character");
         }
-        //extentTest.pass("completing registration");
+
+
 //        Click on Register to complete registration as a vendor
         JSUtils.clickWithTimeoutByJS(pearlyMarketLoginRegisterPage.registerButton);
-        MediaUtils.takeScreenshotOfTheEntirePageAsString();
-        Driver.closeDriver();
-    }
 
+//        Verify that “This Email already exists. Please login to the site and apply as vendor.”  message appears
+//        if user tries to register with a registered email
+        //WaitUtils.waitForVisibility(pearlyMarketLoginRegisterPage.errorMessage_registeredEmail,5);
+        ReusableMethods.verifyElementDisplayed(WaitUtils.waitForVisibility(pearlyMarketLoginRegisterPage.errorMessage_registeredEmail,5));
+
+        Driver.closeDriver();
+
+    }
 }
